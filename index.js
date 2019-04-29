92e4e10281d970e266fa0708154b0995d350af66
Min Kang <kangm914@gmail.com>
2019-04-29 18:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,5 +53,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if(cardNumber === 0) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var string = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+    return string;
+  }
 }
